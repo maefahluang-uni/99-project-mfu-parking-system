094b48eb30cd50578664b2b00e1e1a303e59482d
yam2547 <6531503034@lamduan.mfu.ac.th>
2023-11-14 21:21:54 +0700

    vehicle class

--- src/main/java/th/mfu/Vehicle.java
+++ src/main/java/th/mfu/Vehicle.java
@@ -1,39 +1,63 @@
 package th.mfu;
-import java.util.ArrayList;
 
-public abstract class Vehicle   {
-protected ArrayList<ParkingSlot> parkingSlots = new ArrayList<ParkingSlot>();  
-  
-  
-protected int slotsNeeded;  
-  
-protected VSize siz;  
-  
-protected String companyName;  
-  
-public int getSlotsNeeded()  
-{  
-return slotsNeeded;  
-}  
-  
-public void parkInSlot(ParkingSlot s)  
-{  
-parkingSlots.add(s);  
-}  
-  
-public VSize getSize()  
-{  
-return siz;  
-}  
-  
-public void clearSlots()  
-{  
-for (int i = 0; i < parkingSlots.size(); i++)  
-{  
-parkingSlots.get(i).removeVehicle();  
-}  
-parkingSlots.clear();  
-}  
-  
-public abstract boolean canFitInSlot(ParkingSlot spot);  
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    private String make;
+    private String model;
+    private String slot;
+
+    // Constructors, getters, and setters
+
+    public Vehicle() {
+        // Default constructor
+    }
+
+    public Vehicle(String make, String model, String slot) {
+        this.make = make;
+        this.model = model;
+        this.slot = slot;
+    }
+
+    // Getters and setters for id, make, model, and slot
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
 }
