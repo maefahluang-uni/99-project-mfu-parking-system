4b5e02c648a3038620d66d932175c7561d9912a8
yam2547 <6531503034@lamduan.mfu.ac.th>
2023-11-24 00:46:51 +0700

    database

--- src/main/java/th/mfu/Building.java
+++ src/main/java/th/mfu/Building.java
@@ -14,6 +14,13 @@ public class Building {
 
     private String namebulid;
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNamebuild(){
         return namebulid;
     }
--- src/main/java/th/mfu/Role.java
+++ src/main/java/th/mfu/Role.java
@@ -1,5 +1,42 @@
 package th.mfu;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String student;
+    private String Staffvisitor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
     
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getstaffvisitor() {
+        return Staffvisitor;
+    }
+
+    public void setStaffvisitor(String staffvisitor) {
+        this.Staffvisitor = staffvisitor;
+    }
+
 }
