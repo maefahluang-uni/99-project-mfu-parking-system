e265ffe6ef63907d5cd405b833588a1613a2fa0a
yam2547 <6531503034@lamduan.mfu.ac.th>
2023-11-26 23:16:27 +0700

    reser

--- src/main/java/th/mfu/Service/ReservationService.java
+++ src/main/java/th/mfu/Service/ReservationService.java
@@ -1,41 +1,41 @@
-// package th.mfu.Service;
+package th.mfu.Service;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import th.mfu.Domain.Reservation;
-// import th.mfu.Repositories.ReservationRepository;
+import th.mfu.Domain.Reservation;
+import th.mfu.Repositories.ReservationRepository;
 
-// @Service
-// public class ReservationService {
+@Service
+public class ReservationService {
 
-//     private ReservationRepository repository;
+    private ReservationRepository repository;
 
-//     private ReservationService(ReservationRepository repository){
-//         this.repository = repository;
-//     }
+    private ReservationService(ReservationRepository repository){
+        this.repository = repository;
+    }
 
-//     public List<Reservation> findAll(){
-//         return repository.findAll();
-//     }
+    public List<Reservation> findAll(){
+        return repository.findAll();
+    }
 
-//     public List<Reservation> findByUserId(Integer userId){
-//         return repository.findByUserID(userId);
-//     }
+    public List<Reservation> findByUserId(Integer userId){
+        return repository.findByUserID(userId);
+    }
 
-//     public Reservation createReservation(Reservation reservation){
-//         return repository.save(reservation);
-//     }
+    public Reservation createReservation(Reservation reservation){
+        return repository.save(reservation);
+    }
 
-//     public Reservation updateReservation(Reservation reservation){
-//         var reservationData = new Reservation();
-//         reservationData.setReserId(reservation.getReserId());
+    public Reservation updateReservation(Reservation reservation){
+        var reservationData = new Reservation();
+        reservationData.setReserId(reservation.getReserId());
 
-//         return repository.save(reservationData);
-//     }
+        return repository.save(reservationData);
+    }
 
-//     public void deleteReservation(Integer userId){
-//         repository.deleteById(userId);
-//     }
-// }
+    public void deleteReservation(Integer userId){
+        repository.deleteById(userId);
+    }
+}
