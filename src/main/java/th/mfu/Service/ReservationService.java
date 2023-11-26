327ad1f0f76a0a0230fbdb94afade769b2a24a6e
Kittapas004 <6531503004@lamduan.mfu.ac.th>
2023-11-27 01:45:14 +0700

    ss

---
+++ src/main/java/th/mfu/Service/ReservationService.java
@@ -0,0 +1,43 @@
+package th.mfu.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import th.mfu.Domain.Reservation;
+import th.mfu.Repositories.ReservationRepository;
+
+@Service
+public class ReservationService {
+
+    @Autowired
+    private ReservationRepository repository;
+
+    private ReservationService(ReservationRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Reservation> findAll(){
+        return repository.findAll();
+    }
+
+    public List<Reservation> findByUserId(Integer userId){
+        return repository.findByUserID(userId);
+    }
+
+    public Reservation createReservation(Reservation reservation){
+        return repository.save(reservation);
+    }
+
+    public Reservation updateReservation(Reservation reservation){
+        var reservationData = new Reservation();
+        reservationData.setReserId(reservation.getReserId());
+
+        return repository.save(reservationData);
+    }
+
+    public void deleteReservation(Integer userId){
+        repository.deleteById(userId);
+    }
+}
